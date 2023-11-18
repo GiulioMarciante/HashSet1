6e586a403a80d64bc15daae4e27f961b817433c4
Giulio Marciante <87944393+undeermind@users.noreply.github.com>
2023-11-18 16:01:51 +0100

    Esercizio HashSet1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+//Scrivere una funzione che restituisca un hashset riempito
+//Leggere l'hashSet e stampre grandezza ed elementi
 public class Main {
     public static void main(String[] args) {
+        System.out.println("The size of the HashSet is: " + hashSetFunction().size() +"\n" + "\nThe elements are the follows:");
+
+        Iterator it = hashSetFunction().iterator();
+        while(it.hasNext()){
+            Object element = it.next();
+            System.out.println("element: " + element);
         }
+    }
+    public static Set hashSetFunction(){
+        return new HashSet<>(Arrays.asList("carrots", "zucchini", "tomatoes"));
+    }
 }
 
